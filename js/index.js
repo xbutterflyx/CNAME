c6cfe2d5675843eedb5757cbd3c8aa07f8b283a0
xbutterflyx <xbutterflyx@163.com>
2018-08-26 12:31:57 +0800

    visitors statistics

--- js/index.js
+++ js/index.js
@@ -1,27 +1,24 @@
 const signs = document.querySelectorAll('x-sign')
 const randomIn = (min, max) => (
-  Math.floor(Math.random() * (max - min + 1) + min)
+	Math.floor(Math.random() * (max - min + 1) + min)
 )
 
 const mixupInterval = el => {
-  const ms = randomIn(2000, 4000)
-  el.style.setProperty('--interval', `${ms}ms`)
+	const ms = randomIn(2000, 4000)
+	el.style.setProperty('--interval', `${ms}ms`)
 }
 
 signs.forEach(el => {
-  mixupInterval(el)
-  el.addEventListener('webkitAnimationIteration', () => {
-    mixupInterval(el)
-  })
+	mixupInterval(el)
+	el.addEventListener('webkitAnimationIteration', () => {
+		mixupInterval(el)
+	})
 })
 
-
-if (localStorage.pagecount)
-	{
-	localStorage.pagecount=Number(localStorage.pagecount) +1;
-	}
-else
-	{
-	localStorage.pagecount=1;
-	}
-document.write("Visits:  " + localStorage.pagecount + "  time(s).");
+Integer count = (Integer) application.getAttribute("count");
+if(count == null) {
+	count = 0;
+}
+application.setAttribute("count", count + 1);
+var num = count
+document.getElementById("num").innerHTML = num;
